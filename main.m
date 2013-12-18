b30ca752975341c27b468db3dd1e1f1ea54da890
Han Zuohao <hanzuohao123@gmail.com>
2013-12-18 19:05:34 +0800

    pic

--- main.m
+++ main.m
@@ -2,16 +2,16 @@ function B = main()
 I=imread('pic/1.jpg');
 I=double(I)/255;
 
-w     = 5;       % bilateral filter half-width
-sigma = [10 0.0425]; % bilateral filter standard deviations
+w     = 3;       % bilateral filter half-width
+sigma = [3 0.1]; % bilateral filter standard deviations
 
 I1=bfilter2(I,w,sigma);
-I1=bfilter2(I1,w,sigma);
 
 imshow(I1);
 I2 = DoG(I1,1.0,1.27,0.98);
 I3 = tanh(I2);
 I4 = combine(I1,I3);
-imshow(I4);
+
+%imshow(I1);
 imwrite(I4,'result.jpg');
 end
