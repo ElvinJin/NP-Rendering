c4b54a854b122aa66ad36ed9beade9c743fffd06
Elvin Jin <elvinjin8@gmail.com>
2013-12-20 07:36:25 +0800

    Finalize application

--- cartoon/DoG.m
+++
@@ -1,5 +0,0 @@
-function d = DoG(img,sigma1,sigma2,t)
-    gaussian1 = fspecial('gaussian',5,sigma1);
-    gaussian2 = fspecial('gaussian',5,sigma2);
-    d = imfilter(img,gaussian1) - imfilter(img,gaussian2) * t;
-    end
--- cartoon/convertLAB.m
+++
@@ -1,7 +0,0 @@
-function [l,a,b] = convertLAB(rgbImage)
-    colorTransform = makecform('srgb2lab');
-    lab = applycform(rgbImage, colorTransform);
-    l = lab(:, :, 1);  % Extract the L image.
-    a = lab(:, :, 2);  % Extract the A image.
-    b = lab(:, :, 3);  % Extract the B image.
-end
